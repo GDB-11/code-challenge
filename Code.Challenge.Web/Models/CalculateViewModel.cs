05a7250cae0299ef70c5c1a5598cae5a897c691d
GianBadoino <gianfrancodiaz011@gmail.com>
2023-10-25 07:40:07 -0500

    Validation messages

--- Code.Challenge.Web/Models/CalculateViewModel.cs
+++ Code.Challenge.Web/Models/CalculateViewModel.cs
@@ -5,12 +5,15 @@ namespace Code.Challenge.Web.Models
     public class CalculateViewModel
     {
         [Required(ErrorMessage = "The first input is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "The first input must be greater than 0.")]
         public int FirstInput { get; set; }
 
         [Required(ErrorMessage = "The second input is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "The second input must be greater than 0.")]
         public int SecondInput { get; set; }
 
         [Required(ErrorMessage = "The sample size input is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "The sample size must be greater than 0.")]
         public int SampleSize { get; set; }
     }
 }
